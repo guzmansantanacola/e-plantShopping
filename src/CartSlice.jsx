12c521581d927eb4f57aaed5024f33899745b195
Guzman <guzmansantanacola@gmail.com>
2024-11-18 13:38:58 -0500

    first

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,21 +3,31 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [], // Lista de productos en el carrito
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const item = action.payload;
+      const existingItem = state.items.find((product) => product.name === item.name);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ ...item, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      const item = action.payload;
+      state.items = state.items.filter((product) => product.name !== item.name);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { item, quantity } = action.payload;
+      const existingItem = state.items.find((product) => product.name === item.name);
+      if (existingItem) {
+        existingItem.quantity = quantity;
+      }
     },
   },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
-
 export default CartSlice.reducer;
